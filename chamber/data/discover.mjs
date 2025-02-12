ee20e55793481b88b87c3a51b408757455ff4d4f
bnewvillage <blainenewvillage@gmail.com>
2025-02-12 15:53:56 +0400

    initial push

---
+++ chamber/data/discover.mjs
@@ -0,0 +1,58 @@
+export const places = [
+        {
+          "title": "Dubai Autodrome",
+          "description": "Experience high-speed karting on both indoor and outdoor tracks, suitable for all skill levels. Offers karting schools, corporate events, and championships.",
+          "address": "Motor City, Dubai, UAE",
+          "imagePath": "images/autodrome.webp",
+          "url": "https://dubaiautodrome.ae/"
+        },
+        {
+          "title": "The Els Club",
+          "description": "Enjoy a round of golf on a championship course designed by Ernie Els. Features 18 holes, a driving range, and various golfing events.",
+          "address": "Dubai Sports City, Dubai, UAE",
+          "imagePath": "images/els.webp",
+          "url" : "https://www.elsclubdubai.com"
+        },
+        {
+          "title": "Dubai Sports City Skate Park",
+          "description": "A haven for skateboarders and BMX riders, this park offers a variety of ramps, rails, and bowls for all levels of enthusiasts.",
+          "address": "Dubai Sports City, Dubai, UAE",
+          "imagePath": "images/skatepark.webp",
+          "url" : "https://www.visitdubai.com/en/places-to-visit/xpark-by-etisalat"
+        },
+        {
+          "title": "Spanish Soccer Schools",
+          "description": "Participate in soccer clinics, training sessions, or watch matches at this academy known for its professional coaching and facilities.",
+          "address": "Dubai Sports City, Dubai, UAE",
+          "imagePath": "images/soccerschool.webp",
+          "url" : "https://iscdubai.sabis.net/about-isc-dubai/media-gallery/spanish-soccer-school"
+        },
+        {
+          "title": "GymNation Motor City",
+          "description": "A modern gym with a wide array of equipment, group fitness classes, and personal training options to cater to all fitness levels.",
+          "address": "Motor City, Dubai, UAE",
+          "imagePath": "images/gymnation.webp",
+          "url" : "https://gymnation.com › gymsnearme › motor-city"
+        },
+        {
+          "title": "Extreme Fun Play Centre",
+          "description": "An ideal spot for families with children, offering soft play areas, bouncy castles, and interactive games for a day of fun.",
+          "address": "Motor City, Dubai, UAE",
+          "imagePath": "images/playcentre.webp",
+          "url" : "https://extremefunuae.com"
+        },
+        {
+          "title": "DSC Canal",
+          "description": "Engage in kayaking, paddle-boarding, or simply enjoy a leisurely boat ride in the serene canal setting within Dubai Sports City.",
+          "address": "Dubai Sports City, Dubai, UAE",
+          "imagePath": "images/canal.webp",
+          "url" : "https://www.dsc.ae › locations › canal-residence-west"
+        },
+        {
+          "title": "FitRepublik",
+          "description": "This upscale fitness center boasts top-notch facilities including an Olympic-sized swimming pool, martial arts classes, and CrossFit sessions.",
+          "address": "Dubai Sports City, Dubai, UAE",
+          "imagePath": "images/fitrepublik.webp",
+          "url" : "https://fitrepublik.com"
+        }
+      ]
---
+++ chamber/scripts/discover.js
@@ -0,0 +1,57 @@
+import {places} from '../data/discover.mjs'
+
+//created cards
+const featuredSection = document.querySelector('#featuredSection');
+places.forEach(place => {
+    let card = document.createElement('section');
+    let title = document.createElement('h2');
+    let description = document.createElement('p');
+    let address = document.createElement('p');
+    let learnBtn = document.createElement('button');
+    let image = document.createElement('img');
+
+    title.innerHTML = `${place.title}`;
+    description.innerHTML = `${place.description}`;
+    address.innerHTML = `${place.address}`;
+    learnBtn.innerHTML = `Learn More`;
+    image.setAttribute('src',place.imagePath);
+    image.setAttribute('width', '300px');
+
+    title.classList.add('title');
+    description.classList.add('description');
+    address.classList.add('address');
+    card.classList.add('card');
+    learnBtn.classList.add('learnBtn');
+    learnBtn.onclick = ()=> {window.open(place.url, '_blank')};
+    image.classList.add('placeImg');
+    
+
+    card.appendChild(title);
+    card.appendChild(image);
+    card.appendChild(address);
+    card.appendChild(description);
+    card.appendChild(learnBtn);
+    console.log(card);
+
+    featuredSection.appendChild(card);
+});
+
+//creating last visit
+const visitorMessage = document.getElementById('visitor-message');
+const lastVisit = localStorage.getItem('lastVisit');
+const now = new Date().getTime();
+
+if (!lastVisit){
+    visitorMessage.textContent = 'Welcome! Let us know if you have any questions.';
+} else {
+    const lastVisitTime = parseInt(lastVisit);
+    const timeDifference = now - lastVisitTime;
+    const daysDifference = Math.floor(timeDifference / (1000*60*60*24));
+    if (daysDifference < 1){
+        visitorMessage.textContent = `Back so soon! Awesome!`
+    } else {
+        visitorMessage.textContent = `You last visited ${daysDifference} days ago!`
+    }
+}
+
+localStorage.setItem('lastVisit', now);
